e5a057d2109980e5f28e572ae676acacc747abe9
waynemamahit <wayne.mamahit@gmail.com>
2024-09-05 05:08:08 +0700

    Setup auth feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')
+    ->prefix('profile')
+    ->controller(ProfileController::class)
+    ->group(function () {
+        Route::get('', 'edit')->name('profile.edit');
+        Route::patch('', 'update')->name('profile.update');
+        Route::delete('', 'destroy')->name('profile.destroy');
+    });
+
+require_once __DIR__ . '/auth.php';
